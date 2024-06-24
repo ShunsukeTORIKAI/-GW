b983810af64686b18eb9769dc46bd2ca63a70257
ShunsukeTORIKAI <torikai-shunsuke972@g.ecc.u-tokyo.ac.jp>
2024-06-24 21:30:46 +0900

    Add files via upload

--- my_nlp_project/src/my_library/count_polarity_statistics.py
+++ my_nlp_project/src/my_library/count_polarity_statistics.py
@@ -1,19 +1,22 @@
-def _analyse(value, table):
-    if value == -1:
-        table[0] += 1
-    elif value == 0:
-        table[1] += 1
-    elif value == 1:
-        table[2] += 1
-    return table 
-
 def count(d1, d2, sentence):
-    PN_table = [0, 0, 0]
-    for word in sentence:
+    PN_table = [0, 0, 0] #陰性、中性、陽性
+    string=""
+    for i in range(len(sentence)):
+    #for word in sentence:
+        word=sentence[i]
+        string+=word
+        if word == "けど":#逆接の助詞
+            PN_table=[0.1*pn for pn in PN_table]
+            #ここの係数については、再考
+        a=1
+        if i+1<len(sentence) and sentence[i+1]=="ない":
+            a=-1 #「ない」の直前では、極性を反転させる
         if word in d1:
-            PN_table = _analyse(d1[word], PN_table)
+            kyokusei=a*d1[word]+1
+            PN_table[kyokusei]+=1
+            string+=str(kyokusei)
         elif word in d2:
-            PN_table = _analyse(d2[word], PN_table)
-        else:
-            pass
+            kyokusei=a*d2[word]+1
+            PN_table[kyokusei]+=1
+            string+=str(kyokusei)
     return PN_table
