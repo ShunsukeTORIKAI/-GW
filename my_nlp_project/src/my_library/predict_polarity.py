cc7052325778384c4c61143e0743c00fb62ad8bb
ShunsukeTORIKAI <torikai-shunsuke972@g.ecc.u-tokyo.ac.jp>
2024-06-24 20:32:20 +0900

    Add files via upload

--- my_nlp_project/src/my_library/predict_polarity.py
+++ my_nlp_project/src/my_library/predict_polarity.py
@@ -2,6 +2,12 @@ def predict(PN_table):
     sum_of_classified_word = PN_table[0] + PN_table[2]    #正、負の極性を持つ語数
     sum_of_polarity = PN_table[2] - PN_table[0] #極性の和
     if sum_of_classified_word != 0:
-        return sum_of_polarity / sum_of_classified_word
+        score_of_polarity = sum_of_polarity / sum_of_classified_word
+        if 0.2 < score_of_polarity <= 1.0:
+            return f"Positive({score_of_polarity})"
+        elif -1.0 <= score_of_polarity < -0.2:
+            return f"Negative({score_of_polarity})"
+        else:
+            return f"Neutral({score_of_polarity})"
     else:
-        return 0
+        return "Not found"
