c558b338d025e85ecad8e1296f6758b2e19f3aa9
ShunsukeTORIKAI <torikai-shunsuke972@g.ecc.u-tokyo.ac.jp>
2024-06-26 17:51:57 +0900

    Delete my_nlp_project/src/my_library/predict_polarity_v2.py

--- my_nlp_project/src/my_library/predict_polarity_v2.py
+++
@@ -1,7 +0,0 @@
-def predict(PN_table):
-    sum_of_classified_word = PN_table[0] + PN_table[2]    #正、負の極性を持つ語数
-    sum_of_polarity = PN_table[2] - PN_table[0] #極性の和
-    if sum_of_classified_word != 0:
-        return sum_of_polarity / sum_of_classified_word
-    else:
-        return 0
