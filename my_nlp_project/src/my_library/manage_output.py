be82398b16faf620863b7c1605373b57e88cebb7
ShunsukeTORIKAI <torikai-shunsuke972@g.ecc.u-tokyo.ac.jp>
2024-06-26 17:50:57 +0900

    Add files via upload

--- my_nlp_project/src/my_library/manage_output.py
+++ my_nlp_project/src/my_library/manage_output.py
@@ -2,5 +2,5 @@ def output(rawdata_arrays, result):    #rawdata_arraysはdata.txtの文字列を
     for i in range(len(rawdata_arrays)):
         sentence_str = rawdata_arrays[i]
         sentence_str += "\t" + str(result[i]) + "\n"
-        with open("../logs/log.txt", "a") as f:
+        with open("../logs/log_v9.txt", "a") as f:
             f.write(sentence_str)
